2938051ffe926c42c56a1fd69a9875ca67620e80
Vivek Katariya <katariyavivek228@gmail.com>
2023-05-05 01:42:40 +0530

    second commit

--- Tik tac toe/ViewController3.swift
+++ Tik tac toe/ViewController3.swift
@@ -1,15 +1,21 @@
 
 
-
-
+//
+//  ViewController.swift
+//  demo game
+//
+//  Created by R81 on 21/04/23.
+//
 
 
 import UIKit
 
 class ViewController3 : UIViewController {
 
+    
+    
     @IBOutlet var buttons: [UIButton]!
-      @IBOutlet weak var xScore: UILabel!
+    @IBOutlet weak var xScore: UILabel!
       @IBOutlet weak var oScore: UILabel!
       @IBOutlet weak var xScoreLabel: UILabel!
       @IBOutlet weak var oScoreLabel: UILabel!
@@ -26,15 +32,16 @@ class ViewController3 : UIViewController {
           super.viewDidLoad()
           board()
           value()
-          //showalert(title: "")
-        // point = score
+         buttonSetup()
       }
       
       @IBAction func buttonsAction(_ sender: UIButton) {
+//          let buttons = sender as! UIButton
+         
           let index = buttons.lastIndex(of: sender)!
-           
           if !boarder[index].isEmpty
           {
+              
               return
           }
           if currentPlayer == "O"
@@ -42,12 +49,16 @@ class ViewController3 : UIViewController {
               sender.setTitle("O", for: .normal)
               currentPlayer = "X"
               boarder[index] = "O"
+              
+//              buttons.backgroundColor = UIColor(red: 198/255, green:27/255, blue:68/255, alpha: 2)
           }
           else
           {
               sender.setTitle("X", for: .normal)
               currentPlayer = "O"
               boarder[index] = "X"
+//              buttons.backgroundColor = UIColor(red: 43/255, green: 89/255, blue: 255/255, alpha: 2)
+//
              
           }
           value()
@@ -65,8 +76,7 @@ class ViewController3 : UIViewController {
                  player1 == player2,
                  !player2.isEmpty
               {
-                 // print(player1)
-                  //print("Win is \(player0)")
+                 
                   showalert(title: "Awasom win is \(player2)!")
                   return
               }
@@ -82,7 +92,7 @@ class ViewController3 : UIViewController {
       func reload()
       {
           boarder.removeAll()
-         // loadView()
+   
           board()
           for k in buttons
           {
@@ -96,15 +106,65 @@ class ViewController3 : UIViewController {
           alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: { _ in
               self.reload()
           }))
-          //alert.addAction()
+         
           present(alert, animated: true, completion: nil)
       }
       func board()
       {
-          for i in 0..<buttons.count
+          for _ in 0..<buttons.count
           {
               boarder.append("")
           }
       }
-      
+    func buttonSetup () {
+      buttons[0].layer.cornerRadius = 35
+      buttons[1].layer.cornerRadius = 35
+      buttons[2].layer.cornerRadius = 35
+      buttons[3].layer.cornerRadius = 35
+      buttons[4].layer.cornerRadius = 35
+      buttons[5].layer.cornerRadius = 35
+      buttons[6].layer.cornerRadius = 35
+      buttons[7].layer.cornerRadius = 35
+      buttons[8].layer.cornerRadius = 35
+      buttons[0].layer.shadowOffset = CGSize(width: 3, height: 3)
+      buttons[0].layer.shadowOpacity = 3
+      buttons[0].layer.shadowColor = UIColor.gray.cgColor
+      buttons[1].layer.shadowOffset = CGSize(width: 5, height: 5)
+      buttons[1].layer.shadowOpacity = 3
+      buttons[1].layer.shadowColor = UIColor.gray.cgColor
+      buttons[2].layer.shadowOffset = CGSize(width: 5, height: 5)
+      buttons[2].layer.shadowOpacity = 3
+      buttons[2].layer.shadowColor = UIColor.gray.cgColor
+      buttons[3].layer.shadowOffset = CGSize(width:5, height: 5)
+      buttons[3].layer.shadowOpacity = 3
+      buttons[3].layer.shadowColor = UIColor.gray.cgColor
+      buttons[4].layer.shadowOffset = CGSize(width: 5, height: 5)
+      buttons[4].layer.shadowOpacity = 3
+      buttons[4].layer.shadowColor = UIColor.gray.cgColor
+      buttons[5].layer.shadowOffset = CGSize(width: 5, height: 3)
+      buttons[5].layer.shadowOpacity = 3
+      buttons[5].layer.shadowColor = UIColor.gray.cgColor
+      buttons[6].layer.shadowOffset = CGSize(width: 3, height: 5)
+      buttons[6].layer.shadowOpacity = 3
+      buttons[6].layer.shadowColor = UIColor.gray.cgColor
+      buttons[7].layer.shadowOffset = CGSize(width: 5, height: 5)
+      buttons[7].layer.shadowOpacity = 3
+      buttons[7].layer.shadowColor = UIColor.gray.cgColor
+      buttons[8].layer.shadowOffset = CGSize(width:5, height: 5)
+      buttons[8].layer.shadowOpacity = 3
+      buttons[8].layer.shadowColor = UIColor.gray.cgColor
+      }
+      func clearColor (){
+          buttons[0].layer.backgroundColor = UIColor.lightGray.cgColor
+          buttons[1].layer.backgroundColor = UIColor.lightGray.cgColor
+          buttons[2].layer.backgroundColor = UIColor.lightGray.cgColor
+          buttons[3].layer.backgroundColor = UIColor.lightGray.cgColor
+          buttons[4].layer.backgroundColor = UIColor.lightGray.cgColor
+          buttons[5].layer.backgroundColor = UIColor.lightGray.cgColor
+          buttons[6].layer.backgroundColor = UIColor.lightGray.cgColor
+          buttons[7].layer.backgroundColor = UIColor.lightGray.cgColor
+          buttons[8].layer.backgroundColor = UIColor.lightGray.cgColor
+  
+      }
+
   }
